381aaa62e717e274ceb2644af984ff4c6acec0e9
Macbook Beks <babdikaarov@gmail.com>
2023-08-03 08:40:10 +0300

    Responsive fix

--- src/containers/MenuBar.js
+++ src/containers/MenuBar.js
@@ -1,12 +1,9 @@
 import React from "react";
 import styles from "../styles/MenuBar.module.css";
 import SpotifyLogOutButton from "../components/SpotifyLogOutButton";
-import SpotifyLogInButton from "../components/SpotifyLogInButton";
-
 export default function MenuBar({ setlogIn }) {
   return (
     <div className={styles.div}>
-      <SpotifyLogInButton setlogIn={setlogIn} />
       <SpotifyLogOutButton setlogIn={setlogIn} />
     </div>
   );
